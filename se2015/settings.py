e982c87de43fa23d5bf34ec9e612b9059a45953a
Quang Le Hong <iamquang95@gmail.com>
2015-09-29 23:41:21 +0700

    Restruct project folder

--- se2015/settings.py
+++ se2015/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
 )
 
-ROOT_URLCONF = 'onlineLearning.urls'
+ROOT_URLCONF = 'se2015.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'onlineLearning.wsgi.application'
+WSGI_APPLICATION = 'se2015.wsgi.application'
 
 
 # Database
