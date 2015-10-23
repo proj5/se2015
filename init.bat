dd3aa649ad4c2d69a17d5b1d96d1440663cc3292
Nguyen Xuan Duc <ducnx_58@vnu.edu.vn>
2015-10-23 22:31:12 +0700

    Add Weekly Meeting 231015

---
+++ init.bat
@@ -0,0 +1,25 @@
+@echo off
+:: !Clear old database
+del db.sqlite3
+
+:: !Create new database
+python manage.py migrate
+
+:: Install fixture
+
+:: username:password
+:: admin:admin
+:: user:user
+python manage.py loaddata auth.json
+python manage.py loaddata users.json
+
+:: Force check pep8 when commit
+set filename=.git/hooks/pre-commit
+echo #!/bin/bash >%filename%
+echo set -e >>%filename%
+echo echo '---------------------------------' >>%filename%
+echo pep8 --exclude=*/migrations/ . >>%filename%
+echo echo '---------------------------------' >>%filename%
+echo python manage.py test >>%filename%
+
+
