0c99441b94bca784fdeb8cb0d1381b3d137de614
Nguyen Xuan Duc <ducnx_58@vnu.edu.vn>
2015-10-27 22:22:08 +0700

    Add basic learning function

---
+++ static/javascripts/learning.controller.js
@@ -0,0 +1,5 @@
+var abc = angular.module('se2015.controllers');
+    
+abc.controller('LearningController', function() {
+    this.gradeId = 1;
+});
---
+++ static/javascripts/question.controller.js
@@ -0,0 +1,50 @@
+
+
+var abc = angular.module('se2015.controllers');
+'use strict';
+
+abc.controller('QuestionController', function($http, $routeParams) { 
+    var vm = this;
+    vm.gradeId = $routeParams.gradeId;            
+    vm.questionId = "1";
+    vm.url = "api/v1/exercises/" + vm.questionId + "/";            
+    
+    vm.answer = "0";
+    vm.submitted = false;    
+    
+    getData();
+    
+    function getData() {       
+        $http.get(vm.url, { headers: {
+                'Content-type': 'application/json'
+                }
+            }).success(function(data, status, headers, config) {               
+                vm.data = JSON.parse(JSON.stringify(data));                   
+                
+        }).error(function(data, status, headers, config) {
+            console.log("Error");
+        });
+    }        
+    vm.change = function() {        
+        var num = Math.floor(Math.random() * 6 + 1);        
+        vm.questionId = num.toString();    
+        vm.url = "api/v1/exercises/" + vm.questionId + "/";            
+        vm.submitted = false;        
+        vm.answer = "0";
+        getData();        
+    };           
+    
+});
+
+abc.factory('myFactory', function($resource) {
+    return $resource('api/v1/exercises/:id', {
+        id : '@id'}, {
+        query: {
+            method: 'GET',
+            params: {
+                id : "1"
+            },        
+            isArray : false
+        }
+    });
+});
