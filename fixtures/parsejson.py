345b01548cf38c6942a10e736f335250709ee621
Quang Le Hong <iamquang95@gmail.com>
2015-11-12 20:51:58 +0700

    Delete unused file

--- fixtures/parsejson.py
+++
@@ -1,9 +0,0 @@
-import json
-
-filename = "users.json"
-
-with open(filename, 'r') as handle:
-    new_json = json.load(handle)
-
-f = open(filename, 'w')
-f.write(json.dumps(new_json, indent=2, sort_keys=True))
