c02b580b6f4d708799817a7de3eeb3506e75334e
namnd_58 <namnd_58@vnu.edu.vn>
2015-12-04 20:37:37 +0700

    Update exercise's test
    Seperate exercise and exam test
    Add test for create exercise

--- exercises/tests.py
+++ exercises/tests.py
@@ -1,12 +1,45 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
+from exercises.models import Exercise, Skill
 
 
 class ExerciseTest(APITestCase):
     fixtures = [
         'auth', 'users', 'grades', 'skills', 'exercises', 'possible_answer',
-        'exams'
     ]
+
+    def test_create_exercise(self):
+        skill = Skill.objects.get(id=1)
+        grade = skill.grade
+
+        skill_num_exercise = skill.num_exercises
+        grade_num_exercise = grade.num_exercises
+
+        exercise = Exercise(
+            question='1+1=',
+            question_type='AN',
+            answer='2',
+            skill=skill
+        )
+        exercise.save()
+
+        # The number of exercise should increase
+        self.assertEqual(skill.num_exercises, skill_num_exercise + 1)
+        self.assertEqual(grade.num_exercises, grade_num_exercise + 1)
+
+    def test_update_exercise(self):
+        exercise = Exercise.objects.get(id=1)
+        skill = exercise.skill
+        grade = skill.grade
+        exercise.answer = '2'
+
+        skill_num_exercise = skill.num_exercises
+        grade_num_exercise = grade.num_exercises
+        exercise.save()
+
+        # The number of exercise should not change
+        self.assertEqual(skill.num_exercises, skill_num_exercise)
+        self.assertEqual(grade.num_exercises, grade_num_exercise)
 
     def test_get_exercise(self):
         url = '/api/v1/exercise/1/1/'
@@ -77,6 +110,13 @@ class ExerciseTest(APITestCase):
             ]
         })
         self.assertEqual(response.data, False)
+
+
+class ExamTest(APITestCase):
+    fixtures = [
+        'auth', 'users', 'grades', 'skills', 'exercises', 'possible_answer',
+        'exams'
+    ]
 
     def test_post_exam_correct(self):
         url = '/api/v1/auth/login/'
